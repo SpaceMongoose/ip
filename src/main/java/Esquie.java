9b4a73881242c855885ee43874f42b0e49e54e3e
Marcus Chan <68299706+SpaceMongoose@users.noreply.github.com>
2026-01-16 02:26:33 +0800

    Add Echo functionality

--- src/main/java/Esquie.java
+++ src/main/java/Esquie.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
+
 /**
- * Esquie, a Personal Assistant Chatbot that helps a person keep track of various things
- * */
+ * A Personal Assistant Chatbot that helps a person keep track of various things.
+ */
 public class Esquie {
     /**
-     * Prints the logo, welcome message and immediately exits
+     * Prints the logo, welcome message and immediately exits.
+     *
      * @param args Command line arguments passed to the program (not used).
      */
     public static void main(String[] args) {
@@ -17,11 +20,27 @@ public class Esquie {
                                 | |            \s
                                 |_|             \
                 """;
-        String breakLine = "\n--------------------------------------";
-        System.out.println(logo + breakLine);
+        String breakLine = "--------------------------------------";
+        String replyBreakLine = "    --------------------------------------";
+        String indentation = "    ";
+        System.out.println(logo + "\n" + breakLine);
 
         // Initial Conversation
         System.out.println("Bonjour mon ami! I'm Esquie\uD83D\uDE00" + "\nWhat can I do for you?");
+        System.out.println(breakLine);
+
+        // Loop to Scan for Inputs
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            } else {
+                System.out.println(indentation + replyBreakLine);
+                System.out.println(indentation + indentation + input);
+                System.out.println(indentation + replyBreakLine);
+            }
+        }
         System.out.println(breakLine);
         System.out.println("Bye mon ami! Hope to see you again soon!");
     }
