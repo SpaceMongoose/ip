10c6e3ecf83662c596e884486360887fb27f1c64
Marcus Chan <68299706+SpaceMongoose@users.noreply.github.com>
2026-01-16 14:30:08 +0800

    Create Task class to track task completion

---
+++ src/main/java/Task.java
@@ -0,0 +1,43 @@
+/**
+ * Task class that contains a description and a completion status.
+ *
+ */
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    /**
+     * Initializes new Task with given description.
+     * The initialized task is not done by default (i.e. isDone = false).
+     *
+     * @param description Text describing the task.
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Returns the current status of completion in String.
+     * "X" indicates completion, " " indicates not complete.
+     *
+     * @return icon "X" or " ".
+     */
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    /**
+     * Marks the task as complete.
+     */
+    public void markComplete() {
+        this.isDone = true;
+    }
+
+    /**
+     * Marks the task as incomplete.
+     */
+    public void markIncomplete() {
+        this.isDone = false;
+    }
+}
