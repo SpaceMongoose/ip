3cdf1475c6ee922224a133e80c1266769446bd13
Marcus Chan <68299706+SpaceMongoose@users.noreply.github.com>
2026-01-16 13:46:15 +0800

    Refactor Welcome and Exit Message to seperate functions

--- src/main/java/Esquie.java
+++ src/main/java/Esquie.java
@@ -4,18 +4,43 @@ import java.util.Scanner;
  * A Personal Assistant Chatbot that helps a person keep track of various things.
  */
 public class Esquie {
-    /** Array for storing tring entered by the user. */
+    /** Array (with 100 limit) for storing tasks entered by the user. */
     private static String[] taskList = new String[100];
 
-    /** Counter for the number of task added. */
+    /** Counter for the number of tasks added. */
     private static int numberOfTasks = 0;
 
-    /**
-     * Prints the logo, welcome message and immediately exits.
-     *
-     * @param args Command line arguments passed to the program (not used).
-     */
+    /** Constants used for standardized formatting. */
+    private static final String BREAKLINE = "--------------------------------------";
+    private static final String REPLYBREAKLINE = "    --------------------------------------";
+    private static final String INDENTATION = "    ";
+
     public static void main(String[] args) {
+
+        // Prints the Welcome Message
+        printWelcome();
+
+        // Loop to Scan for Inputs
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            } else {
+                taskList[numberOfTasks++] = input;
+                System.out.println(INDENTATION + REPLYBREAKLINE);
+                System.out.println(INDENTATION + INDENTATION + "added: " + input);
+                System.out.println(INDENTATION + REPLYBREAKLINE);
+            }
+        }
+        printExit();
+    }
+
+    /**
+     * Prints the logo and welcome message to the User.
+     *
+     */
+    public static void printWelcome() {
         String logo = """
                   ______                 _     \s
                  |  ____|               (_)    \s
@@ -26,29 +51,20 @@ public class Esquie {
                                 | |            \s
                                 |_|             \
                 """;
-        String breakLine = "--------------------------------------";
-        String replyBreakLine = "    --------------------------------------";
-        String indentation = "    ";
-        System.out.println(logo + "\n" + breakLine);
+
+        System.out.println(logo + "\n" + BREAKLINE);
 
         // Initial Conversation
         System.out.println("Bonjour mon ami! I'm Esquie\uD83D\uDE00" + "\nWhat can I do for you?");
-        System.out.println(breakLine);
+        System.out.println(BREAKLINE);
+    }
 
-        // Loop to Scan for Inputs
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            String input = sc.nextLine();
-            if (input.equalsIgnoreCase("bye")) {
-                break;
-            } else {
-                taskList[numberOfTasks++] = input;
-                System.out.println(indentation + replyBreakLine);
-                System.out.println(indentation + indentation + "added: " + input);
-                System.out.println(indentation + replyBreakLine);
-            }
-        }
-        System.out.println(breakLine);
+    /**
+     * Prints the exit message.
+     *
+     */
+    public static void printExit() {
+        System.out.println(BREAKLINE);
         System.out.println("Bye mon ami! Hope to see you again soon!");
     }
 }
