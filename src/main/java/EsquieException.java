64b7ee55298564f04ce8992c6db7be98b25f7e93
Marcus Chan <68299706+SpaceMongoose@users.noreply.github.com>
2026-01-17 16:10:44 +0800

    Add EsquieException class

---
+++ src/main/java/EsquieException.java
@@ -0,0 +1,14 @@
+/**
+ * EsquieException represents exceptions specific to Esquie
+ */
+public class EsquieException extends Exception {
+
+    /**
+     * Initializes new error specific to Esquie with the following error message
+     *
+     * @param message Text describing the error
+     */
+    public EsquieException(String message) {
+        super(message);
+    }
+}
