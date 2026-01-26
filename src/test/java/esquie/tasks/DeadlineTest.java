ef7d705e6b3dfbbcd04bfb5374f2b55539a44f9e
Marcus Chan <68299706+SpaceMongoose@users.noreply.github.com>
2026-01-26 18:10:27 +0800

    DeadlineTest.java: Add JUnit test for Deadline.java

---
+++ src/test/java/esquie/tasks/DeadlineTest.java
@@ -0,0 +1,40 @@
+package esquie.tasks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeadlineTest {
+
+    @Test
+    public void deadline_add_success() {
+        Deadline deadline = new Deadline("Read book", "2026-01-01");
+        assertEquals("[D][ ] Read book (by: 1 Jan 2026)", deadline.toString());
+
+        Deadline deadlineWithTime = new Deadline("Read book", "2026-01-01 1300");
+        assertEquals("[D][ ] Read book (by: 1 Jan 2026, 1300H)", deadlineWithTime.toString());
+    }
+
+    @Test
+    public void deadline_add_saveString() {
+        Deadline deadline = new Deadline("Read book", "2026-01-01");
+        assertEquals("D | 0 | Read book | 2026-01-01 0000", deadline.saveString());
+
+        Deadline deadlineWithTime = new Deadline("Read book", "2026-01-01 1300");
+        assertEquals("D | 0 | Read book | 2026-01-01 1300", deadlineWithTime.saveString());
+    }
+
+    @Test
+    public void deadline_manual_mark() {
+        Deadline deadlineMarked = new Deadline("Read book", "2026-01-01", true);
+        Deadline deadlineUnmarked = new Deadline("Read book", "2026-01-01", false);
+
+        //  the print and save string for a marked task
+        assertEquals("[D][X] Read book (by: 1 Jan 2026)", deadlineMarked.toString());
+        assertEquals("D | 1 | Read book | 2026-01-01 0000", deadlineMarked.saveString());
+
+        //  the print and save string for a unmarked task
+        assertEquals("[D][ ] Read book (by: 1 Jan 2026)", deadlineUnmarked.toString());
+        assertEquals("D | 0 | Read book | 2026-01-01 0000", deadlineUnmarked.saveString());
+    }
+}
