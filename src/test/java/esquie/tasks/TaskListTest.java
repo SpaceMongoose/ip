ace2ef4236bafafafc1c3247903482cf78bf78eb
Marcus Chan <68299706+SpaceMongoose@users.noreply.github.com>
2026-01-26 20:22:08 +0800

    StorageTest.java: Add JUnit test for Storage.java

--- src/test/java/esquie/tasks/TaskListTest.java
+++ src/test/java/esquie/tasks/TaskListTest.java
@@ -1,10 +1,8 @@
 package esquie.tasks;
 
-import esquie.exceptions.EsquieException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class TaskListTest {
