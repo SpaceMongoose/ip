5a093e4f6b95c6d5eaed8c8418814fc915078595
Marcus Chan <68299706+SpaceMongoose@users.noreply.github.com>
2026-01-26 18:37:58 +0800

    EventTest.java: Add JUnit test for Event.java

---
+++ src/test/java/esquie/tasks/EventTest.java
@@ -0,0 +1,50 @@
+package esquie.tasks;
+
+import esquie.exceptions.EsquieException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class EventTest {
+
+    @Test
+    public void event_add_success() throws EsquieException {
+        Event event = new Event("Read book", "2026-01-01", "2026-01-02");
+        assertEquals("[E][ ] Read book (from: 1 Jan 2026 to: 2 Jan 2026)", event.toString());
+
+        Event eventWithTime = new Event("Read book", "2026-01-01 1000", "2026-01-02 2359");
+        assertEquals("[E][ ] Read book (from: 1 Jan 2026, 1000H to: 2 Jan 2026, 2359H)",
+                    eventWithTime.toString());
+    }
+
+    @Test
+    public void event_add_saveString() throws EsquieException {
+        Event event = new Event("Read book", "2026-01-01", "2026-01-02");
+        assertEquals("E | 0 | Read book | 2026-01-01 0000 | 2026-01-02 0000", event.saveString());
+
+        Event eventWithTime = new Event("Read book", "2026-01-01 1000", "2026-01-02 2359");
+        assertEquals("E | 0 | Read book | 2026-01-01 1000 | 2026-01-02 2359", eventWithTime.saveString());
+    }
+
+    @Test
+    public void event_manual_mark() {
+
+    }
+
+    @Test
+    public void event_initialization_failure() {
+        EsquieException exception = assertThrows(EsquieException.class, () -> {
+            new Event("Read book", "2026-01-02", "2026-01-01");
+            });
+
+        assertEquals("Oopsie! End time cannot be before Start time!", exception.getMessage());
+
+        EsquieException exceptionWithTime = assertThrows(EsquieException.class, () -> {
+            new Event("Read book", "2026-01-01 2359", "2026-01-01 0000");
+        });
+
+        assertEquals("Oopsie! End time cannot be before Start time!", exceptionWithTime.getMessage());
+    }
+}
