691366cc6d3c1fe4469cadbb4ca79368301cc69f
Marcus Chan <68299706+SpaceMongoose@users.noreply.github.com>
2026-01-26 17:42:22 +0800

    TodoTest.java: Add JUnit test for Todo.java

---
+++ src/test/java/esquie/tasks/TodoTest.java
@@ -0,0 +1,34 @@
+package esquie.tasks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodoTest {
+
+    @Test
+    public void todo_add_success() {
+        Todo todo = new Todo("Read book");
+        assertEquals("[T][ ] Read book", todo.toString());
+    }
+
+    @Test
+    public void todo_add_saveString() {
+        Todo todo = new Todo("Read book");
+        assertEquals("T | 0 | Read book", todo.saveString());
+    }
+
+    @Test
+    public void todo_manual_mark() {
+        Todo todoMarked = new Todo("Read book" , true);
+        Todo todoUnmarked = new Todo("Read book" , false);
+
+        //  the print and save string for a marked task
+        assertEquals("[T][X] Read book", todoMarked.toString());
+        assertEquals("T | 1 | Read book", todoMarked.saveString());
+
+        //  the print and save string for a unmarked task
+        assertEquals("[T][ ] Read book", todoUnmarked.toString());
+        assertEquals("T | 0 | Read book", todoUnmarked.saveString());
+    }
+}
