e3d1a548f62d6b87317c8bc86a6661f733d6d9ad
Marcus Chan <68299706+SpaceMongoose@users.noreply.github.com>
2026-02-11 01:05:01 +0800

    Refactor: Improve code quality in Storage class
    * Extract magic literals and strings to Message class
    * Rework loadTask() to use a functional approach with parseString(),
    reducing deep nesting.

--- src/main/java/esquie/common/Messages.java
+++ src/main/java/esquie/common/Messages.java
@@ -4,6 +4,19 @@ package esquie.common;
  * Class for user-visible messages.
  */
 public class Messages {
+    // ERROR EXAMPlES
+    public static final String ERR_TODO_EXAMPLE = "Example Usage: todo borrow book";
+    public static final String ERR_DEADLINE_EXAMPLE = "Example Usage: deadline Play E33 /by 2026-01-25 1750"
+            + "\n" + "Example Usage: deadline Play E33 /by 2026-01-25";
+
+    public static final String ERR_DEADLINE_DATE_EXAMPLE = "Example Usage: /by 2026-01-25 OR /by 2026-01-25 1750";
+    public static final String ERR_EVENT_EXAMPLE = "Example Usage: event Play E33 /from 2026-01-25 1300 "
+            + "/to 2026-01-25 1800" + "\n" + "Example Usage: event Play E33 /from 2026-01-25 /to 2026-01-25";
+
+    public static final String ERR_DELETE_EXAMPLE = "Example Usage: delete 3";
+    public static final String ERR_MARK_EXAMPLE = "Example Usage: mark 1 OR unmark 1";
+    public static final String ERR_FIND_EXAMPLE = "Example Usage: find book";
+
 
     // ERR MESSAGE
     public static final String ERR_TASKLIST_FULL = "Whoopsie! Number of tasks is full!";
@@ -23,22 +36,16 @@ public class Messages {
 
     // TODO ERROR MESSAGE
     public static final String ERR_TODO_FORMAT = "Whoopsie! Something is missing from the todo command!"
-            + "\n" + "Example Usage: todo borrow book";
+            + "\n" + ERR_TODO_EXAMPLE;
 
     // DEADLINE ERROR MESSAGE
     public static final String ERR_DEADLINE_FORMAT = "Whoopsie! Something is missing from the deadline command!"
-            + "\n" + "Example Usage: deadline Play E33 /by 2026-01-25 1750"
-            + "\n" + "Example Usage: deadline Play E33 /by 2026-01-25";
+            + "\n" + ERR_DEADLINE_EXAMPLE;
 
-    public static final String ERR_DEADLINE_DATE = "Oopsie! Please enter the date in yyyy-MM-dd or yyyy-MM-dd HHmm "
-            + "format!" + "\n" + "Example Usage: /by 2026-01-25 OR /by 2026-01-25 1750";
+    public static final String ERR_DEADLINE_DATE = "Whoopsie! Please enter the date in yyyy-MM-dd or yyyy-MM-dd HHmm "
+            + "format!" + "\n" + ERR_DEADLINE_DATE_EXAMPLE;
 
     // EVENT ERROR MESSAGE
-    private static final String ERR_EVENT_EXAMPLE = "Example Usage: event Play E33 /from 2026-01-25 1300 "
-            + "/to 2026-01-25 1800"
-            + "\n"
-            + "Example Usage: event Play E33 /from 2026-01-25 /to 2026-01-25";
-
     public static final String ERR_EVENT_FORMAT = "Whoopsie, something is wrong with the event command!"
             + "\n" + "Either a task description or time is missing!" + "\n"
             + ERR_EVENT_EXAMPLE;
@@ -47,12 +54,12 @@ public class Messages {
             + "\n" + "Either the from or to timing is missing from the event command!" + "\n"
             + ERR_EVENT_EXAMPLE;
 
-    public static final String ERR_EVENT_DATE = "Oopsie! Please enter the date in yyyy-MM-dd or yyyy-MM-dd HHmm format!"
+    public static final String ERR_EVENT_DATE = "Whoopsie! Please enter the date in yyyy-MM-dd "
+            + "or yyyy-MM-dd HHmm format!"
             + "\n"
             + ERR_EVENT_EXAMPLE;
 
     // DELETE ERROR MESSAGE
-    private static final String ERR_DELETE_EXAMPLE = "Example Usage: delete 3";
     public static final String ERR_DELETE_FORMAT = "Whoopsie! Something is wrong with the delete command!"
             + "\n"
             + ERR_DELETE_EXAMPLE;
@@ -62,7 +69,6 @@ public class Messages {
             + ERR_DELETE_EXAMPLE;
 
     // MARK ERROR MESSAGE
-    private static final String ERR_MARK_EXAMPLE = "Example Usage: mark 1 OR unmark 1";
     public static final String ERR_MARK_FORMAT = "Whoopsie! command is missing an argument!"
             + "\n"
             + ERR_MARK_EXAMPLE;
@@ -71,10 +77,16 @@ public class Messages {
             + ERR_MARK_EXAMPLE;
 
     // FIND ERROR MESSAGE
-    private static final String ERR_FIND_EXAMPLE = "Example Usage: find book";
     public static final String ERR_FIND_FORMAT = "Whoopsie! You have to tell me what to find!"
             + "\n"
             + ERR_FIND_EXAMPLE;
+
+    // STORAGE ERROR MESSAGE
+    public static final String ERR_STORAGE_CORRUPT_SKIP = "Warning! Skipping corrupted line in save file: ";
+    public static final String ERR_STORAGE_CORRUPT_FILE = "Whoopsie! Corrupted Save File!";
+    public static final String ERR_STORAGE_IMPORT = "Whoopsie! Something went wrong with importing previous tasks!";
+    public static final String ERR_STORAGE_SAVE = "Whoopsie! Something went wrong with the save file!";
+    public static final String ERR_STORAGE_WRITE = "Whoopsie! Something went wrong with writing to the save file!";
 
 
     private Messages() {};
