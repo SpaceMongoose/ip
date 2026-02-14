1753cbc602492a6e8a77bf332727a4e6780f25d8
Marcus Chan <68299706+SpaceMongoose@users.noreply.github.com>
2026-02-14 15:51:19 +0800

    A-Personality: Update Esquie personality

--- src/main/java/esquie/common/Messages.java
+++ src/main/java/esquie/common/Messages.java
@@ -24,8 +24,8 @@ public class Messages {
     public static final String ERR_TASK_NOT_FOUND = "Whoopsie! Nothing could be found";
 
     // SUCCESS MESSAGE
-    public static final String MSG_TASK_ADD = "Got it. I've added this task:";
-    public static final String MSG_TASK_DELETE = "Got it. I've removed this task:";
+    public static final String MSG_TASK_ADD = "Got it, mon ami. I've added this task:";
+    public static final String MSG_TASK_DELETE = "Got it, mon ami. I've removed this task:";
     public static final String MSG_TASK_LIST = "Listing Current Tasks:";
     public static final String MSG_TASK_FIND = "Here are the matching tasks in your list:";
     public static final String MSG_TASK_MARK = "WhooWhee! I've marked this task as done:";
--- src/main/java/esquie/ui/Ui.java
+++ src/main/java/esquie/ui/Ui.java
@@ -21,70 +21,16 @@ public class Ui {
     }
 
     /**
-     * Reads and returns input from the user.
-     */
-    public String readCommand() {
-        return sc.nextLine().trim();
-    }
-
-    /**
-     * Prints the logo and welcome message to the User.
-     *
-     */
-    public void printWelcome() {
-        String logo = """
-                  ______                 _     \s
-                 |  ____|               (_)    \s
-                 | |__   ___  __ _ _   _ _  ___\s
-                 |  __| / __|/ _` | | | | |/ _ \\
-                 | |____\\__ \\ (_| | |_| | |  __/
-                 |______|___/\\__, |\\__,_|_|\\___|
-                                | |            \s
-                                |_|             \
-                """;
-
-        System.out.println(logo + "\n" + BREAKLINE);
-
-        // Initial Conversation
-        System.out.println("Bonjour mon ami! I'm Esquie\uD83D\uDE00" + "\nWhat can I do for you?");
-        System.out.println(BREAKLINE);
-    }
-
-    /**
-     * Prints the exit message.
-     *
-     */
-    public void printExit() {
-        System.out.println(DOUBLEINDENTATION + "Bye mon ami! Hope to see you again soon!");
-    }
-
-    /**
-     * Prints an indented line that acts as a separator.
-     */
-    public void printLine() {
-        System.out.println(DOUBLEINDENTATION + BREAKLINE);
-    }
-
-    /**
      * Prints a formatted error message.
      */
     public void printError(String message) {
-        // System.out.println(DOUBLEINDENTATION + message);
         response.append(message).append("\n");
-    }
-
-    /**
-     * Returns a DOUBLEINDENTATION for formatting reasons.
-     */
-    public String printIndent() {
-        return DOUBLEINDENTATION;
     }
 
     /**
      * Prints a formatted message.
      */
     public void showMessage(String message) {
-        // System.out.println(DOUBLEINDENTATION + message);
         response.append(message).append("\n");
     }
 
