d9cb97df3b661c7fc3018524177191abad29fafb
Marcus Chan <68299706+SpaceMongoose@users.noreply.github.com>
2026-02-20 23:28:58 +0800

    Ui: Update minimum window size dimension

--- src/main/java/esquie/Main.java
+++ src/main/java/esquie/Main.java
@@ -35,8 +35,8 @@ public class Main extends Application {
             stage.setTitle("Esquie Bot");
             Image image = new Image(this.getClass().getResourceAsStream("/images/e33_icon.jpg"));
             stage.getIcons().add(image);
-            stage.setMinWidth(300);
-            stage.setMinHeight(300);
+            stage.setMinWidth(400);
+            stage.setMinHeight(600);
 
             // 6. Inject Esquie
             Esquie esquie = new Esquie("./data/esquie.txt");
